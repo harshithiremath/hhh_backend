686563a056884ff2992ce2b60a38a2b13f9008c0
harshithiremath <harshit.hir2011@gmail.com>
2020-05-11 23:02:05 +0530

    Fixes #1 and Fixes #2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let cors = require("cors");
 const bodyParser = require("body-parser");
 const connection = require("./app/models/db.js");
 const app = express();
+
 app.use(cors());
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
@@ -64,30 +65,74 @@ app.get("/orders", (req, res) => {
   // console.log(req);
   console.log("user", user);
   // console.log(`SELECT * FROM users WHERE email='${user}'`);
-  connection.query(`SELECT * FROM users WHERE email='${user}'`, function (
-    err,
-    useless_res,
-    fields
-  ) {
-    if (err) {
-      res.send({ message: "error in query1" });
-    } else {
-      // console.log(useless_res[0].user_id);
-      console.log(
-        `SELECT * FROM merchandise_order WHERE user_id='${useless_res[0].user_id}'`
-      );
-      connection.query(
-        `SELECT * FROM merchandise_order WHERE user_id='${useless_res[0].user_id}'`,
-        function (err, results, fields) {
-          if (err) {
-            res.send({ message: "error in query2" });
-          } else {
-            res.send(results);
+  connection.query(
+    `SELECT * FROM users WHERE email='${user}'`,
+    (err, used_res, fields) => {
+      if (err) {
+        res.send({ message: "error in query1" });
+      } else if (used_res.length === 0) {
+        res.send({});
+      } else {
+        // console.log(useless_res[0].user_id);
+        console.log(
+          `SELECT * FROM merchandise_order WHERE user_id='${used_res[0].user_id}'`
+        );
+        connection.query(
+          `SELECT * FROM merchandise_order WHERE user_id='${used_res[0].user_id}'`,
+          function (err, results, fields) {
+            if (err) {
+              res.send({ message: "error in query2" });
+            } else {
+              res.send(results);
+            }
           }
-        }
-      );
+        );
+      }
     }
-  });
+  );
+});
+
+// ! route
+app.get("/bought_tickets", (req, res) => {
+  const user = req.query.user;
+  // console.log("user", user);
+  connection.query(
+    `SELECT * FROM users WHERE email='${user}'`,
+    (err, res1, fields) => {
+      if (err) {
+        res.send({ message: "error in bought_tickets1" });
+      } else if (res1.length === 0) {
+        res.send({});
+      } else {
+        connection.query(
+          `SELECT * FROM ticket_purchase WHERE user_id='${res1[0].user_id}'`,
+          (err, results, fields) => {
+            if (err) {
+              res.send({ message: "error in bought_tickets2" });
+            } else {
+              res.send(results);
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+// ! route
+app.get("/singleTour", (req, res) => {
+  const tour_id = req.query.tour_id;
+  console.log("tour_id", tour_id);
+  connection.query(
+    `SELECT * FROM tours WHERE tour_id='${tour_id}'`,
+    (err, res1, fields) => {
+      if (err) {
+        res.send({ message: "error in singleTour get1" });
+      } else {
+        res.send(res1);
+      }
+    }
+  );
 });
 
 //! route
@@ -133,13 +178,13 @@ app.post("/checkout/tickets", (req, res) => {
 });
 
 //! route
-app.post("/cart", (res, req) => {
+app.post("/cart", (req, res) => {
   data = req.body;
-  //details:(Object conatining essential credentials)
+  //details:(Object containing essential credentials)
   //data.details.user_id
   //data.details.merch_id
   //data.details.quantity
-  if (data.message === "insert") {
+  if (data["message"] === "insert") {
     connection.query(
       "SELECT quantity FROM merchandise_cart WHERE user_id=? and merch_id= ?",
       [data.details.user_id, data.details.merch_id],
@@ -174,7 +219,7 @@ app.post("/cart", (res, req) => {
       }
     );
   }
-  if (data.message === "view") {
+  if (data["message"] === "view") {
     connection.query(
       "SELECT m.merch_id, m.quantity, p.price*m.quantity AS price FROM merch p, merchandise_cart m WHERE user_id= ? and m.merch_id=p.merch_id ",
       [data.details.user_id],
