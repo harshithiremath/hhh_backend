6c3ba5f509c5d322e3d9df6564daed205538f88f
harshithiremath <harshit.hir2011@gmail.com>
2020-05-17 00:51:39 +0530

    /cart increment and /decrement added

---
+++ app/routes/cart.routes.js
@@ -0,0 +1,161 @@
+module.exports = (app, connectio) => {
+  // ! route
+  app.post("/cart", (req, res) => {
+    let data = req.body;
+    // console.log("data in req in cart1", data);
+    // // details:(Object containing essential credentials)
+    // // data.details.user_id
+    // // data.details.merch_id
+    // // data.details.quantity
+    // // data.details.merch_limit
+    if (data.message === "insert") {
+      connectio.query(
+        `SELECT * FROM merchandise_cart WHERE user_id=(SELECT user_id from users where users.email='${data.details.user_id}') and merch_id=${data.details.merch_id}`,
+        (err, res1) => {
+          // // console.log("res in cart1", res);
+          if (err) {
+            console.log("error in sql query 1 ");
+          } else {
+            if (res1.length === 0) {
+              // // console.log("res in cart1", res);
+              // // console.log(
+              // //  `INSERT INTO merchandise_cart(user_id, merch_id, quantity) values ((SELECT user_id from users WHERE email='${data.details.user_id}'), ${data.details.merch_id}, ${data.details.quantity})`
+              // // );
+
+              connectio.query(
+                `INSERT INTO merchandise_cart(user_id, merch_id, quantity) values ((SELECT user_id from users WHERE email='${data.details.user_id}'), ${data.details.merch_id}, ${data.details.quantity})`,
+                (err, rws) => {
+                  if (err) console.log("error in sql query 2 ");
+                  app.post("/checkout/merchandise");
+                  console.log("inserted in cart");
+                }
+              );
+              res.send("Item added to cart");
+              return;
+            } else {
+              // // console.log(res1[0].quantity);
+              let quantity = res1[0].quantity;
+              // quantity += data.details.quantity;
+              if (quantity + data.details.quantity > data.details.merch_limit) {
+                quantity = data.details.merch_limit;
+              }
+              // // console.log(
+              // //   "quantity",
+              // //   quantity,
+              // //   "merch_id",
+              // //   data.details.merch_id
+              // // );
+
+              connectio.query(
+                `UPDATE merchandise_cart SET quantity=${quantity} WHERE user_id=(SELECT user_id from users where users.email='${data.details.user_id}') and merch_id=${data.details.merch_id}`,
+                (err, res1) => {
+                  if (err) {
+                    console.log("quantity not updated");
+                  } else {
+                    res.send("Item added to cart");
+                  }
+                }
+              );
+
+              return;
+            }
+          }
+        }
+      );
+    }
+    if (data.message === "view") {
+      connectio.query(
+        `SELECT m.merch_id, p.merch_limit, p.price AS individual_price, p.merch_name, p.image_url, m.quantity, p.price*m.quantity AS price
+        FROM merch p, merchandise_cart m 
+        WHERE m.user_id=(SELECT user_id FROM users WHERE email='${data.details.email}') and m.merch_id=p.merch_id`,
+        (err, resu) => {
+          if (err) throw err;
+          if (resu) {
+            res.send(resu);
+          }
+          if (!resu) {
+            res.send("cart is empty");
+          }
+        }
+      );
+    }
+  });
+
+  // ! route
+
+  app.post("/decrementCart", (req, res) => {
+    connectio.query(
+      `SELECT * FROM merchandise_cart WHERE user_id=(SELECT user_id from users WHERE email='${req.body.body.email}') and merch_id='${req.body.body.merch_id}'`,
+      (err, res1, fields) => {
+        // console.log(res1);
+        if (err) console.log("error in decrementCart");
+        else {
+          if (res1.length > 0) {
+            let json = JSON.parse(JSON.stringify(res1));
+            let tempQuantity = res1[0].quantity;
+            if (tempQuantity > 1) {
+              connectio.query(
+                `UPDATE merchandise_cart SET quantity=${
+                  tempQuantity - 1
+                } WHERE cart_item_id=${res1[0].cart_item_id}`,
+                (err, res2) => {
+                  if (err) {
+                    console.log("error in query2");
+                    // res.send("error in query 2");
+                  } else {
+                    // res.send("decremented successfully!");
+                  }
+                }
+              );
+            } else {
+              connectio.query(
+                `DELETE FROM merchandise_cart WHERE cart_item_id=${res1[0].cart_item_id}`,
+                (err, res2) => {
+                  if (err) {
+                    console.log("error in query2");
+                    // res.send("error in query 2");
+                  } else {
+                    // res.send("deleted successfully!");
+                  }
+                }
+              );
+            }
+          }
+        }
+      }
+    );
+    res.send("deremented/deleted");
+  });
+  app.post("/incrementCart", (req, res) => {
+    connectio.query(
+      `SELECT * FROM merchandise_cart c, merch m WHERE c.user_id=(SELECT user_id from users WHERE email='${req.body.body.email}') and c.merch_id=${req.body.body.merch_id} and c.merch_id=m.merch_id`,
+      (err, res1) => {
+        // console.log(res1);
+        if (err) console.log("error in incrementCart");
+        else {
+          if (res1.length > 0) {
+            let json = JSON.parse(JSON.stringify(res1));
+            let tempQuantity = res1[0].quantity;
+            // console.log(res1[0].merch_limit);
+            if (tempQuantity < res1[0].merch_limit) {
+              connectio.query(
+                `UPDATE merchandise_cart SET quantity=${
+                  tempQuantity + 1
+                } WHERE cart_item_id=${res1[0].cart_item_id}`,
+                (err, res2) => {
+                  if (err) {
+                    console.log("error in query2");
+                    // res.send("error in query 2");
+                  } else {
+                    // res.send("decremented successfully!");
+                  }
+                }
+              );
+            }
+          }
+        }
+      }
+    );
+    res.send("deremented/deleted");
+  });
+};
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get("/", (req, res) => {
 require("./app/routes/user.routes.js")(app);
 // require("./app/routes/tour.routes")(app);
 
+require("./app/routes/cart.routes.js")(app, connection);
 //! route
 app.get("/tours", (req, res) => {
   connection.query("SELECT * FROM tours", function (err, results, fields) {
@@ -35,7 +36,7 @@ app.get("/tours", (req, res) => {
 app.get("/merch", (req, res) => {
   //! This route is used both for getting the details of all the merchandise
   //! and of a single merchandise whose merch_id is passed in query object of HTTP GET request
-  // console.log(req);
+  // // console.log(req);
   if (Object.keys(req.query).length != 0) {
     const merch_id = req.query.merch_id;
     connection.query(
@@ -62,9 +63,8 @@ app.get("/merch", (req, res) => {
 //! route
 app.get("/orders", (req, res) => {
   const user = req.query.user;
-  // console.log(req);
-  console.log("user", user);
-  // console.log(`SELECT * FROM users WHERE email='${user}'`);
+  // // console.log("user", user);
+  // // console.log(`SELECT * FROM users WHERE email='${user}'`);
   connection.query(
     `SELECT * FROM users WHERE email='${user}'`,
     (err, used_res, fields) => {
@@ -73,10 +73,10 @@ app.get("/orders", (req, res) => {
       } else if (used_res.length === 0) {
         res.send({});
       } else {
-        // console.log(useless_res[0].user_id);
-        console.log(
-          `SELECT * FROM merchandise_order WHERE user_id='${used_res[0].user_id}'`
-        );
+        // // console.log(useless_res[0].user_id);
+        // // console.log(
+        // //   `SELECT * FROM merchandise_order WHERE user_id='${used_res[0].user_id}'`
+        // // );
         connection.query(
           `SELECT * FROM merchandise_order WHERE user_id='${used_res[0].user_id}'`,
           function (err, results, fields) {
@@ -95,7 +95,7 @@ app.get("/orders", (req, res) => {
 // ! route
 app.get("/bought_tickets", (req, res) => {
   const user = req.query.user;
-  // console.log("user", user);
+  // // console.log("user", user);
   connection.query(
     `SELECT * FROM users WHERE email='${user}'`,
     (err, res1, fields) => {
@@ -122,7 +122,7 @@ app.get("/bought_tickets", (req, res) => {
 // ! route
 app.get("/singleTour", (req, res) => {
   const tour_id = req.query.tour_id;
-  console.log("tour_id", tour_id);
+  // console.log("tour_id", tour_id);
   connection.query(
     `SELECT * FROM tours WHERE tour_id='${tour_id}'`,
     (err, res1, fields) => {
@@ -178,79 +178,14 @@ app.post("/checkout/tickets", (req, res) => {
 });
 
 //! route
-app.post("/cart", (req, res) => {
-  let data = req.body;
-  // console.log("data in req in cart1", data);
-  // // details:(Object containing essential credentials)
-  // // data.details.user_id
-  // // data.details.merch_id
-  // // data.details.quantity
-  if (data.message === "insert") {
-    connection.query(
-      `SELECT quantity FROM merchandise_cart WHERE user_id=(SELECT user_id from users where users.email='${data.details.user_id}') and merch_id=${data.details.merch_id}`,
-      (err, res1) => {
-        // // console.log("res in cart1", res);
-        if (err) {
-          console.log("error in sql query 1 ");
-        } else {
-          // // console.log(res);
-          if (!res1) {
-            // // console.log("res in cart1", res);
-            // console.log(
-            //   `INSERT INTO merchandise_cart(user_id, merch_id, quantity) values ((SELECT user_id from users WHERE email='${data.details.user_id}'), ${data.details.merch_id}, ${data.details.quantity})`
-            // );
-            connection.query(
-              `INSERT INTO merchandise_cart(user_id, merch_id, quantity) values ((SELECT user_id from users WHERE email='${data.details.user_id}'), ${data.details.merch_id}, ${data.details.quantity})`,
-              (err, rws) => {
-                if (err) throw err;
-                app.post("/checkout/merchandise");
-                console.log("inserted in cart");
-              }
-            );
-            res.send("Item added  to cart ");
-            return;
-          } else {
-            let quantity = res1[0].quantity || res1[0];
-            quantity += data.details.quantity;
-            connection.query(
-              `UPDATE merchandise_cart SET quantity=${quantity} WHERE user_id=(SELECT user_id from users where users.email='${data.details.user_id}') and merch_id=${data.details.merch_id}`,
-              (err, res) => {
-                if (err) console.log("quantity updated");
-              }
-            );
-            res.send("Item added to cart");
-            return;
-          }
-        }
-      }
-    );
-  }
-  if (data["message"] === "view") {
-    connection.query(
-      "SELECT m.merch_id, m.quantity, p.price*m.quantity AS price FROM merch p, merchandise_cart m WHERE m.user_id= ? and m.merch_id=p.merch_id ",
-      [data.details.user_id],
-      (err, resu) => {
-        if (err) throw err;
-        if (resu) {
-          res.send(resu);
-        }
-        if (!resu) {
-          res.send("cart is empty");
-        }
-      }
-    );
-  }
-});
-
-//! route
 app.post("/checkout/merch", (req, res) => {
   data = req.body;
   dtails = data.details;
-  //details must be an array of arrays in which the inner array must have the values in order
-  //details.user_id
-  //details.merch_id
-  //details.quantity
-  //details.timestamp
+  // ? details must be an array of arrays in which the inner array must have the values in order
+  // ? details.user_id
+  // ? details.merch_id
+  // ? details.quantity
+  // ? details.timestamp
   connection.query(
     "INSERT INTO orders_list values ?",
     [dtails],
