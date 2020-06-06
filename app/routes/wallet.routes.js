1c9f797dc757af8ce3120416ca807dc017e914d3
HarshShankar <harshshankar.cs18@bmsce.ac.in>
2020-06-06 11:51:26 +0530

    Added Stripe API

--- app/routes/wallet.routes.js
+++ app/routes/wallet.routes.js
@@ -1,3 +1,4 @@
+const stripe=require("stripe")("sk_test_51GqJIvEkPqBpQLilF1MKxPVKJwtC4DztasQrhoqY9BB89NMRhxxsYnJJEW8FPe4OreHqFrRq1xbylfPgwHRZ5AmE00zTOMudSs")
 module.exports = (app, connection) => {
   // app.get("/singleTour", (req, res) => {
   //     const tour_id = req.query.tour_id;
@@ -72,4 +73,61 @@ module.exports = (app, connection) => {
       }
     );
   });
+  const postStripeCharge = (res,email) => (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      console.log("error in rechargeWallet");
+          res.sendStatus(201);
+    } else {
+      console.log("Done")
+      connection.query(
+        `SELECT user_id FROM users WHERE email='${email}'`,
+        (err, res1) => {
+          if (err) {
+            console.log("error in rechargeWallet 1");
+            res.sendStatus(201);
+          } else {
+            const user_id = res1[0].user_id;
+            // console.log("user_id", res1[0].user_id);
+            // TODO increase the wallet by amount
+            // console.log(
+            //   `UPDATE wallet SET balance=balance+${req.body.amount}, expiry=DATE_ADD(expiry, INTERVAL 2 MONTH) WHERE user_id=${user_id}`
+            // );
+            connection.query(
+              `UPDATE wallet SET balance=balance+${1000}, expiry=DATE_ADD(expiry, INTERVAL 2 MONTH) WHERE user_id=${user_id}`,
+              (err, res2) => {
+                if (err) {
+                  console.log("error in rechargeWallet 2");
+                  res.sendStatus(201);
+                } else {
+                  console.log("successfully recharged the wallet");
+                  res.sendStatus(200);
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  }
+  app.post("/rechargeStripe",async (req,res)=>{
+    console.log("Request :", req.body);
+
+    let error;
+    let status;
+    const { token } = req.body;
+    const customer = await stripe.customers.create({
+      source: 'tok_visa',
+      email: token.email
+    });
+
+    const charge = await stripe.charges.create(
+      {
+        amount: 1000 * 100,
+        currency: "INR",
+        customer: customer.id,
+        description: `Added to walllet`,
+      },
+      postStripeCharge(res,token.email)
+    );
+  })
 };
