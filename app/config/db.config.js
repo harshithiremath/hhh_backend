e9ed3a397d844e23df7d6e3af63345277e37b52c
harshithiremath <harshit.hir2011@gmail.com>
2020-08-30 14:23:20 +0530

    some random changes. god knows what

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "rootpasswordgiven",
   DB: "hhh",
 };
--- server.js
+++ server.js
@@ -9,11 +9,8 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/hhh",(req,res)=>{
-  res.status(418).send({
-    message: "hello"
-  })
-})
+
+// !!!! route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to hhh test application." });
 });
@@ -21,8 +18,9 @@ app.get("/", (req, res) => {
 require("./app/routes/user.routes.js")(app);
 // require("./app/routes/tour.routes")(app);
 
+// !!!! route
 app.get("/tours", (req, res) => {
-  connection.query("SELECT * from tours", function (err, results, fields) {
+  connection.query("SELECT * FROM tours", function (err, results, fields) {
     if (err) {
       res.send({ message: "error in query" });
     } else {
@@ -30,15 +28,67 @@ app.get("/tours", (req, res) => {
     }
   });
 });
+
+// !!!! route
 app.get("/merch", (req, res) => {
-  connection.query("SELECT * from merch", function (err, results, fields) {
+  // todo This route is used both by for getting the details of all the merchandise
+  // todo and of a single merchandise whose merch_id is passed in query object of HTTP GET request
+  // // console.log(req);
+  if (Object.keys(req.query).length != 0) {
+    const merch_id = req.query.merch_id;
+    connection.query(
+      `SELECT * FROM merch WHERE merch_id='${merch_id}'`,
+      function (err, results, fields) {
+        if (err) {
+          res.send({ message: "error in query for individual merch" });
+        } else {
+          res.send(results);
+        }
+      }
+    );
+  } else {
+    connection.query("SELECT * FROM merch", function (err, results, fields) {
+      if (err) {
+        res.send({ message: "error in query" });
+      } else {
+        res.send(results);
+      }
+    });
+  }
+});
+
+// !!!! route
+app.get("/orders", (req, res) => {
+  const user = req.query.user;
+  // console.log(req);
+  console.log("user", user);
+  // console.log(`SELECT * FROM users WHERE email='${user}'`);
+  connection.query(`SELECT * FROM users WHERE email='${user}'`, function (
+    err,
+    useless_res,
+    fields
+  ) {
     if (err) {
-      res.send({ message: "error in query" });
+      res.send({ message: "error in query1" });
     } else {
-      res.send(results);
+      // console.log(useless_res[0].user_id);
+      console.log(
+        `SELECT * FROM merchandise_order WHERE user_id='${useless_res[0].user_id}'`
+      );
+      connection.query(
+        `SELECT * FROM merchandise_order WHERE user_id='${useless_res[0].user_id}'`,
+        function (err, results, fields) {
+          if (err) {
+            res.send({ message: "error in query2" });
+          } else {
+            res.send(results);
+          }
+        }
+      );
     }
   });
 });
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000.");
 });
